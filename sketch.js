95ffe323b03ff95b25f8c1799b3257ad67c496ff
kamya-shah <kamya.shah.sod24@anu.edu.in>
2025-09-17 12:15:40 +0530

    add cat subclass
    i want to add and create subclass of cat in zoo

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  //create Black Cat
+  let blackCat = new Cat("Blacky, straight");
+  let redCat = new Cat("Tiger");
+  
+  //let them talk
+  blackCat.talk();
+  blackCat.expressYourself();
+  
+  redCat.talk();
+}
